38c8480700fd984319eca1430cc2b46586d2fef0
梁世慶 Mars Liang <marsfantasy@gmail.com>
2020-06-06 20:07:41 +0800

    init

---
+++ RealWorldApp/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldApp
+{
+    public static class AppSettings
+    {
+        //public static string ApiUrl = "https://localhost:44333/";
+        public static string ApiUrl = "https://foodapi.azurewebsites.net/";
+    }
+}
